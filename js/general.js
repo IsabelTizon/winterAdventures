0576fe5082a1ae98dc30b4b2899791eebcbf744f
IsabelTizon <tizonarranz@gmail.com>
2022-11-10 20:31:42 +0000

    sign in and sign up pages and icon account navbar

---
+++ js/general.js
@@ -0,0 +1,6 @@
+document.getElementById("goToSignUp").onclick = function () {
+	location.href = "sign-up.html";
+};
+document.getElementById("goToSignIn").onclick = function () {
+	location.href = "sign-in.html";
+};
