1d7114b908f30eaea219ff0c28d44f9dbaaf6c69
IsabelTizon <tizonarranz@gmail.com>
2022-11-13 17:55:31 +0000

    Update general-pages.js

--- js/general-pages.js
+++ js/general-pages.js
@@ -1,6 +1,6 @@
 document.getElementById("goToSignUp").onclick = function () {
-	location.href = "./sign-up.html";
+	location.href = "../pages/sign-up.html";
 };
 document.getElementById("goToSignIn").onclick = function () {
-	location.href = "./sign-in.html";
+	location.href = "../pages/sign-in.html";
 };
